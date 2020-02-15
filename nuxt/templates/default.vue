af1232c1d3f152227088ce4cdd1f31e60296a2ac
Jamie Warburton <jamie@hexdigital.com>
2020-02-15 12:43:21 +0000

    [template-support] Add support for templates on pages in WordPress

---
+++ nuxt/templates/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <BaseTitle>Default Template</BaseTitle>
+    <FlexibleContent :post="post" />
+    <TemporaryPostData :post="post" />
+  </div>
+</template>
+
+<script>
+import FlexibleContent from '~/components/AcfBlocks/FlexibleContent.vue';
+import TemporaryPostData from '~/components/TemporaryPostData.vue';
+
+export default {
+  name: 'DefaultTemplate',
+  components: { FlexibleContent, TemporaryPostData },
+  props: {
+    post: { type: [Array, Object], default: () => ({}) },
+  },
+};
+</script>
---
+++ wp-content/themes/nuxtpress/templates/example-template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * An example template page that connects to a template defined in the WordPress theme
+ *
+ * @package Hex Digital
+ * @subpackage NuxtPress
+ * @since 2020
+ *
+ * Template Name: Example Template
+ */
